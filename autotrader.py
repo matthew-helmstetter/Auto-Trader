119d628a4a2269829a02f1b3db5472ccca7be4fb
matthew-helmstetter <mhelmstetter@berkeley.edu>
2020-08-04 12:05:35 -0700

    fixed calculate_buy_quantity bug

--- autotrader.py
+++ autotrader.py
@@ -13,11 +13,13 @@ def main ():
 def login():
 	return RobinhoodCrypto(username, password)
 
+# Returns quatity of bitcoin to buy. Done in cents only works with a min amount of 1.50
+# TODO fix this later to work with lower denominations
 def calculate_buy_quantity(robinhood_client, amount_to_spend):
 	quote_info = robinhood_client.quotes()
-	price = round(float(quote_info['ask_price']), 2)
-	quantity = amount_to_spend/price
-	return quantity
+	price = round(float(quote_info['mark_price']) * 1.005, 2)
+	quantity = amount_to_spend/(price*100)
+	return str(round(quantity, 8))
 
 # Finds BTC then calculates total yield if sold
 def calculate_sell_profit(robinhood_client):
@@ -32,7 +34,6 @@ def calculate_sell_profit(robinhood_client):
 	return quote_price*float(quantity) - float(capital)
 
 # Returns percentage yield from captial investment
-# TODO possibly make it so it doesnt take inputs 
 def calculate_percent_yield(robinhood_client):
 	quote_info = robinhood_client.quotes()
 	quote_price = round(float(quote_info['bid_price']), 2)
@@ -44,23 +45,16 @@ def calculate_percent_yield(robinhood_client):
 				capital = cost_base['direct_cost_basis']
 	return (quote_price*float(quantity) - float(capital))/float(capital) * 100
 
-# TODO sell ALL bitcoin
+# TODO throw error if there is no BTC
 def sell_all_bitcoin(robinhood_client):
 	quote_info = robinhood_client.quotes()
 	quote_price = round(float(quote_info['bid_price']), 2)
-	# market_order_info = robinhood_client.trade(
-	#     'BTCUSD',
-	#     price=quote_price,
-	#     quantity="0.00005",
-	#     side="buy",
-	#     time_in_force="gtc",
-	#     type="market"
-	# )
-	pass
-# FFUnction used to sell set amount of bitcoin
-def sell_bitcoin(robinhood_client, quantity)
-	quote_info = robinhood_client.quotes()
-	quote_price = round(float(quote_info['bid_price']), 2)
+	holdings_info = robinhood_client.holdings()
+	for asset in holdings_info:
+		for cost_base in asset['cost_bases']:
+			if cost_base['currency_id'] == '1072fc76-1862-41ab-82c2-485837590762':
+				quantity = cost_base['direct_quantity']
+	return quantity
 	# market_order_info = robinhood_client.trade(
 	#     'BTCUSD',
 	#     price=quote_price,
@@ -69,21 +63,38 @@ def sell_bitcoin(robinhood_client, quantity)
 	#     time_in_force="gtc",
 	#     type="market"
 	# )
-
 	
-# buy BTC but do not return true until it has actually bought
-def buy_bitcoin():
+# Function used to sell set amount of bitcoin
+def sell_bitcoin(robinhood_client, quantity):
 	quote_info = robinhood_client.quotes()
-	quote_price = round(float(quote_info['ask_price']), 2)
-	# market_order_info = robinhood_client.trade(
-	#     'BTCUSD',
-	#     price=quote_price,
-	#     quantity="0.00005",
-	#     side="buy",
-	#     time_in_force="gtc",
-	#     type="market"
-	# )
-	pass
+	quote_price = round(float(quote_info['bid_price']), 2)
+	market_order_info = robinhood_client.trade(
+	    'BTCUSD',
+	    price=quote_price,
+	    quantity=quantity,
+	    side="sell",
+	    time_in_force="gtc",
+	    type="market"
+	)
+
+
+# buy BTC but do not return true until it has actually bought
+def buy_bitcoin(robinhood_client, quantity):
+	quote_info = robinhood_client.quotes()
+	market_order_info = robinhood_client.trade(
+	    'BTCUSD',
+	    price=round(float(quote_info['mark_price']) * 1.005, 2),
+	    quantity=str(quantity),
+	    side="buy",
+	    time_in_force="gtc",
+	    type="market"
+	)
+	return market_order_info
+
+def test_buy_bitcoin(robinhood_client):
+	robinhood_client = login()
+	quantity = calculate_buy_quantity(robinhood_client, 100)
+
 
 # if bought below ask_price won't sell instantly
 # quote_info = robinhood_client.quotes()
